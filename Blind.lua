a3660d254e83487a4d3dc2ad16ac9ba5e0d2bb8e
iimateiYT <128510177+iimateiYT@users.noreply.github.com>
2023-03-27 15:48:56 +0100

    Create Blind.lua

---
+++ Blind.lua
@@ -0,0 +1,12 @@
+local Atmosphere = Instance.new("Atmosphere")
+local Blur = Instance.new("Blur")
+
+Blur.Size = 999
+Blur.Parent = game.Lighting
+
+Atmosphere.Density = 1
+Atmosphere.Glare = 10
+Atmosphere.Haze = 10
+Atmosphere.Color = Color3.new(0, 0, 0)
+Atmosphere.Decay = Color3.new(0, 0, 0)
+Atmosphere.Parent = game.Lighting
