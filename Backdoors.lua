95da591d5308330b789a84bc50631aa2722eae32
iimateiYT <128510177+iimateiYT@users.noreply.github.com>
2024-03-17 14:55:57 +0000

    Update Backdoors.lua

--- Backdoors.lua
+++ Backdoors.lua
@@ -1,13 +1,19 @@
+if _G.Executed then
+	warn("Already executed!")
+	return
+end
+_G.Executed = true
+
 if _G.IY then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
 end
-	
+
 local screen_gui = Instance.new("ScreenGui")
+screen_gui.DisplayOrder = 999999999
 screen_gui.IgnoreGuiInset = false
 screen_gui.ResetOnSpawn = false
 screen_gui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 screen_gui.Parent = game.CoreGui
-screen_gui.DisplayOrder = 999999999
 
 local frame = Instance.new("Frame")
 frame.BackgroundColor3 = Color3.new(1, 1, 1)
@@ -47,18 +53,22 @@ template.Name = "template"
 template.Parent = frame
 
 local uiscale = Instance.new("UIScale")
-uiscale.Scale = 0
 uiscale.Parent = template
 
 local uicorner = Instance.new("UICorner")
 uicorner.Parent = template
 
 local uistroke = Instance.new("UIStroke")
+uistroke.Color = Color3.new(0.588235, 0.588235, 0.588235)
 uistroke.Thickness = 2.5
 uistroke.Parent = template
 
+local uigradient = Instance.new("UIGradient")
+uigradient.Color = ColorSequence.new({ColorSequenceKeypoint.new(0, Color3.new(0.996078, 0.0392157, 0)), ColorSequenceKeypoint.new(0.1599999964237213, Color3.new(1, 0.615686, 0)), ColorSequenceKeypoint.new(0.3199999928474426, Color3.new(1, 1, 0)), ColorSequenceKeypoint.new(0.47999998927116394, Color3.new(0, 1, 0)), ColorSequenceKeypoint.new(0.6399999856948853, Color3.new(0, 0.666667, 1)), ColorSequenceKeypoint.new(0.800000011920929, Color3.new(1, 0.333333, 1)), ColorSequenceKeypoint.new(1, Color3.new(0.666667, 0, 1))})
+uigradient.Parent = uistroke
+
 local text_label = Instance.new("TextLabel")
-text_label.Font = Enum.Font.FredokaOne
+text_label.Font = Enum.Font.GothamBold
 text_label.Text = "Entity spawned"
 text_label.TextColor3 = Color3.new(1, 1, 1)
 text_label.TextScaled = true
@@ -70,11 +80,43 @@ text_label.BorderColor3 = Color3.new(0, 0, 0)
 text_label.BorderSizePixel = 0
 text_label.Size = UDim2.new(1, 0, 1, 0)
 text_label.Visible = true
+text_label.ZIndex = 3
 text_label.Parent = template
 
 local uistroke_2 = Instance.new("UIStroke")
 uistroke_2.Thickness = 2.5
 uistroke_2.Parent = text_label
+
+local uipadding_2 = Instance.new("UIPadding")
+uipadding_2.PaddingBottom = UDim.new(0.10000000149011612, 0)
+uipadding_2.PaddingLeft = UDim.new(0.10000000149011612, 0)
+uipadding_2.PaddingRight = UDim.new(0.10000000149011612, 0)
+uipadding_2.PaddingTop = UDim.new(0.10000000149011612, 0)
+uipadding_2.Parent = text_label
+
+local uigradient_2 = Instance.new("UIGradient")
+uigradient_2.Color = ColorSequence.new({ColorSequenceKeypoint.new(0, Color3.new(1, 1, 1)), ColorSequenceKeypoint.new(0.3799654543399811, Color3.new(1, 1, 1)), ColorSequenceKeypoint.new(0.5, Color3.new(0.580392, 0.580392, 0.580392)), ColorSequenceKeypoint.new(0.5336787700653076, Color3.new(1, 1, 1)), ColorSequenceKeypoint.new(1, Color3.new(1, 1, 1))})
+uigradient_2.Offset = Vector2.new(-1, 0)
+uigradient_2.Rotation = 45
+uigradient_2.Parent = template
+
+local shadow = Instance.new("ImageLabel")
+shadow.Image = "rbxassetid://14001321443"
+shadow.ImageColor3 = Color3.new(0, 0, 0)
+shadow.ImageTransparency = 0.75
+shadow.ScaleType = Enum.ScaleType.Slice
+shadow.SliceCenter = Rect.new(50, 50, 150, 150)
+shadow.AnchorPoint = Vector2.new(0.5, 0.5)
+shadow.BackgroundColor3 = Color3.new(1, 1, 1)
+shadow.BackgroundTransparency = 1
+shadow.BorderColor3 = Color3.new(0, 0, 0)
+shadow.BorderSizePixel = 0
+shadow.Position = UDim2.new(0.5, 0, 0.5, 0)
+shadow.Size = UDim2.new(1, 35, 1, 35)
+shadow.Visible = true
+shadow.ZIndex = -1
+shadow.Name = "shadow"
+shadow.Parent = template
 
 function sound()
 	local notification = Instance.new("Sound", workspace)
@@ -97,6 +139,22 @@ function notify(entity, bypassed, other)
 	tem.Parent = frame
 	game:GetService("TweenService"):Create(tem.UIScale, TweenInfo.new(0.25, Enum.EasingStyle.Sine), { Scale = 1 }):Play()
 	spawn(function()
+		while true do
+			task.wait(0.01)
+			tem.UIStroke.UIGradient.Rotation += 1
+		end
+	end)
+
+	spawn(function()
+		while true do
+			task.wait(0.01)
+			if tem.UIGradient.Offset.X >= 1 then
+				tem.UIGradient.Offset = Vector2.new(-1, 0)
+			end
+			tem.UIGradient.Offset += Vector2.new(0.01, 0)
+		end
+	end)
+	spawn(function()
 		task.wait(15)
 		if tem and tem:FindFirstChild("UIScale") then
 			game:GetService("TweenService"):Create(tem.UIScale, TweenInfo.new(0.25, Enum.EasingStyle.Sine), { Scale = 0 }):Play()
@@ -105,12 +163,6 @@ function notify(entity, bypassed, other)
 		end
 	end)
 end
-
-if _G.Executed then
-	notify("", nil, "Already Executed!")
-	return
-end
-_G.Executed = true
 
 local bypassed = false
 
@@ -127,6 +179,10 @@ function selection(child)
 	end
 end
 
+if workspace.CurrentRooms:FindFirstChild("0") then
+	selection(workspace.CurrentRooms["0"].Assets.KeyObtain)
+end
+
 workspace.ChildAdded:Connect(function(child)
 	if child.Name == "BackdoorLookman" then
 		if bypassed then
@@ -137,13 +193,13 @@ workspace.ChildAdded:Connect(function(child)
 		notify("👁️ Backdoor Lookman")
 		selection(child:WaitForChild("Core"))
 	elseif child.Name == "BackdoorRush" then
-		notify("💨 Backdoor Rush")
+		notify("💨 Blitz (Backdoor Rush)")
 		selection(child:WaitForChild("Main"))
 	end
 end)
 
 for _, v in pairs(workspace.CurrentRooms:GetDescendants()) do
-	if v:IsA("BasePart") and v.Name == "Door" and v.Parent.Name == "Door" then
+	if v:IsA("BasePart") and v.Name == "Door" and v.Parent.Name == "Door" or v.Name == "TimerLever" then
 		selection(v)
 	end
 end
@@ -152,6 +208,8 @@ workspace.CurrentRooms.ChildAdded:Connect(function(child)
 	task.wait(1)
 	if child:FindFirstChild("Door") and child.Door:FindFirstChild("Door") then
 		selection(child.Door.Door)
+	elseif child.Assets:FindFirstChild("TimerLever") then
+		selection(child.Assets.TimerLever)
 	end
 end)
 
@@ -160,7 +218,7 @@ notify("", nil, "🐦 Script made by: kiwib.")
 if _G.Bypass then
 	bypassed = true
 	notify("👁️ Backdoor Lookman", true)
-	notify("⚡ Haste", true)
+	notify("", nil, "⚡ Haste is now server-sided so it cannot be bypassed, annoying visuals removed.")
 	game:GetService("ReplicatedStorage").FloorClientStuff.ClientRemote.Haste:Destroy()
 	while true do
 		task.wait()
